98b51413b12088c20c5f81d9d70b781fb4164452
Wuehrl-Christoph <christoph-wuehrl@gmx.de>
2023-06-30 21:51:19 +0200

    add DIPPID event and cursors

--- 02_multitouch_application/multitouch-demo.py
+++ 02_multitouch_application/multitouch-demo.py
@@ -4,6 +4,8 @@ import sys
 
 import pyglet
 
+from DIPPID import SensorUDP
+
 # TODO
 # click detection on sprite
 # receive dippida
@@ -11,7 +13,19 @@ import pyglet
 # shape manipulation
 # distinguish gestures
 
+PORT = 5700
+
 window = pyglet.window.Window(fullscreen=True)
+sensor = SensorUDP(PORT)
+# list to store the received events (should be reset after the events are read)
+current_events = []
+
+class Event:
+    def __init__(self, type, x, y):
+        self.type = type
+        self.x = x
+        self.y = y
+        
 
 
 def init():
@@ -52,9 +66,42 @@ def on_key_press(symbol, modifiers):
 
 @window.event
 def on_draw():
+    window.clear()
+    get_dippid_events()
+    update_cursors()
     batch.draw()
 
+# receives dippid events and stores them in the current event list
+def get_dippid_events():
+    global current_events
+    current_events = []
+    events = sensor.get_value('events')
+    if(events != None):
+        for event in events:
+            type = events[event]['type']
+            x = events[event]['x']
+            y = events[event]['y']
+            x = x * window.width
+            y = window.height - (y * window.height)
+            current_events.append(Event(type, x, y))
+
+def update_cursors():
+    global cursors
+    cursors = []
+    cursor_group = pyglet.graphics.Group(order=2)
+    print(cursors)
+    for event in current_events:
+        touch_event = False
+        if(event.type == 'touch'):
+            touch_event = True
+        if touch_event:
+            cursors.append(pyglet.shapes.Circle(event.x, event.y, 10, color=(0, 0, 255), batch= batch, group= cursor_group))
+        else:
+            cursors.append(pyglet.shapes.Circle(event.x, event.y, 20, color=(0, 255, 0), batch= batch, group= cursor_group))
 
 if __name__ == '__main__':
     init()
     pyglet.app.run()
+
+
+
