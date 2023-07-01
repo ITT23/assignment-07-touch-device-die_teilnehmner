6d3aaffd04943ad869882146bc6da0c92c558d13
Raphael Wagner <31208448+raalwa@users.noreply.github.com>
2023-07-01 20:17:01 +0200

    Add constants

--- 03_gesture_based_application_launcher/config.py
+++ 03_gesture_based_application_launcher/config.py
@@ -1,2 +1,6 @@
 LABELS = ['arrow', 'caret', 'check', 'circle', 'delete_mark', 'left_curly_brace', 'left_sq_bracket', 'pigtail',
           'question_mark', 'rectangle', 'right_curly_brace', 'right_sq_bracket', 'star', 'triangle', 'v', 'x']
+IP = '127.0.0.1'
+PORT = 5700
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
