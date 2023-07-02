2bbc3724bef4e6ff1b7524b64d93bcddc723e8d7
Wuehrl-Christoph <christoph-wuehrl@gmx.de>
2023-07-02 20:38:16 +0200

    fix bugs

--- 02_multitouch_application/multitouch-demo-with-interaction.py
+++ 02_multitouch_application/multitouch-demo-with-interaction.py
@@ -1,30 +1,48 @@
+'''
+This module launches a program that receives input events in the DIPPID-format form the touch box and can be used to manipulate images with it.
+
+When you touch with one finger on an image you can move it around.
+
+When you touch with two fingers on an image you can scale it by using a pinch gesture and rotate it with the rotation of your fingers.
+
+A blue circle indicates a touch event, a green circle indicates a hover event.
+'''
 import math
 import os
 import random
 import sys
-
+import numpy as np
 import pyglet
 from DIPPID import SensorUDP
+import config
 
-# TODO
-# click detection on sprite
-# receive dippida
-# render cursors according to dippid events
-# shape manipulation
-# distinguish gestures
+TOUCH_RADIUS = 10
+HOVER_RADIUS = 20
+SCALING_FACTOR = 0.008
+MOVE_REJECTION = 50
 
-PORT = 5700
-
-# window = pyglet.window.Window(fullscreen=True)
-window = pyglet.window.Window(800, 600)
-sensor = SensorUDP(PORT)
-# list to store the received events (should be reset after the events are read)
+window = pyglet.window.Window(fullscreen=True)
+sensor = SensorUDP(config.PORT)
+'''
+list to store the received events, is reset after the events are read
+'''
 current_events = []
+'''
+dict to store the images with corresponding events
+'''
+current_events_dict = {
+
+}
+'''
+dict to safe the last events when new events are detected to recognize gestures
+'''
 last_events = {
 
 }
 
-
+'''
+Event class to represent the DIPPID events
+'''
 class Event:
     def __init__(self, type, x, y):
         self.type = type
@@ -32,6 +50,9 @@ class Event:
         self.y = y
 
 
+'''
+This method is used to load the images in the 'img' folder and display them in random positions and with random rotations
+'''
 def init():
     global batch, sprites, backgound_group, foreground_group
     batch = pyglet.graphics.Batch()
@@ -52,18 +73,17 @@ def init():
                                        (window.width-sprite.width//2))
                 y_pos = random.randint(sprite.height//2,
                                        window.height-sprite.height//2)
-                sprite.update(x=x_pos, y=y_pos)
+                random_rotation = random.randint(0, 359)
+                sprite.update(x=x_pos, y=y_pos, rotation=random_rotation)
                 sprites.append(sprite)
                 last_events[id(sprite)] = []
 
-
-# Idee:
-# Wenn man auf einen sprite clickt fügt man ihn zur foreground gruppe hinzu
-# => wird vor allen anderen sprites dargestellt
-# Click auf anderen Sprite:
-# => alle Sprites zur Gruppe background, geclickter sprite zur gruppe foreground
-# https://pyglet.readthedocs.io/en/latest/modules/graphics/#pyglet.graphics.Group
-
+'''
+This method checks if an event (represented by an x- and y-coordinate) lies in any picture.
+It returns the picture the event is in and 'None' if there is no picture at this point.
+Sprites with events are grouped in the foreground, sprites with no events are grouped in a background group
+Idea from: https://pyglet.readthedocs.io/en/latest/modules/graphics/#pyglet.graphics.Group
+'''
 def get_selected_sprite(x_pos: int, y_pos: int) -> pyglet.sprite.Sprite | None:
     selected_sprite = None
     for sprite in sprites:
@@ -73,8 +93,9 @@ def get_selected_sprite(x_pos: int, y_pos: int) -> pyglet.sprite.Sprite | None:
         width = sprite.width
         angle = sprite.rotation / 180 * np.pi
 
-        # rotate pos counterclockwise around center
-
+        '''
+        Rotate the coordinates of the event when the image is rotated to check if the are in the rotated image
+        '''
         rotated_x_pos = x + math.cos(angle) * \
             (x_pos - x) - math.sin(angle) * (y_pos - y)
         rotated_y_pos = y + math.sin(angle) * \
@@ -101,18 +122,21 @@ def on_key_press(symbol, modifiers):
     if symbol == pyglet.window.key.Q:
         sys.exit(0)
 
-
+'''
+In every draw cycle the program checks for new DIPPID-events, updates the cursor positions and checks if the events are in an image.
+'''
 @window.event
 def on_draw():
     window.clear()
     get_dippid_events()
-    check_events_in_sprite()
     update_cursors()
+    check_events()
+
     batch.draw()
 
-# receives dippid events and stores them in the current event list
-
-
+'''
+This method checks for DIPPID-events, calculates the absolute positions, and writes them in the current event list
+'''
 def get_dippid_events():
     global current_events
     current_events = []
@@ -126,7 +150,9 @@ def get_dippid_events():
             y = window.height - (y * window.height)
             current_events.append(Event(type, x, y))
 
-
+'''
+This method creates a 'cursors' list and fills it with circles to represent the DIPPID-events
+'''
 def update_cursors():
     global cursors
     cursors = []
@@ -136,122 +162,88 @@ def update_cursors():
         if (event.type == 'touch'):
             touch_event = True
         if touch_event:
-            cursors.append(pyglet.shapes.Circle(event.x, event.y, 10, color=(
+            '''
+            Touch events are represented by a smaller blue circle
+            '''
+            cursors.append(pyglet.shapes.Circle(event.x, event.y, TOUCH_RADIUS, color=(
                 0, 0, 255), batch=batch, group=cursor_group))
         else:
-            cursors.append(pyglet.shapes.Circle(event.x, event.y, 20, color=(
+            '''
+            Hover events are represented by a bigger green circle
+            '''
+            cursors.append(pyglet.shapes.Circle(event.x, event.y, HOVER_RADIUS, color=(
                 0, 255, 0), batch=batch, group=cursor_group))
 
+'''
+This method handles the events and image manipulation. It checks for every event if it is positioned in a image.
+If there is one touch event in a sprite the image will be moved.
+If there are two fingers in a sprite the image will be scaled and rotated
+'''
+def check_events():
+    global current_events_dict, last_events, current_events
+    last_events = current_events_dict
+    current_events_dict = {}
+    for event in current_events:
+        if(event.type != 'touch'):
+            continue
+        sprite = get_selected_sprite(event.x, event.y)
+        if(sprite == None):
+            continue
+        if id(sprite) in current_events_dict:
+            current_events_dict[id(sprite)].append(event)
+        else:
+            current_events_dict[id(sprite)] = [event]
+    for key in current_events_dict:
+        if key in current_events_dict and key in last_events:
+            if (len(current_events_dict[key]) == 1 and len(last_events[key]) == 1):
+                move_picture(last_events[key][0], current_events_dict[key][0], get_sprite(key))
+            elif(len(current_events_dict[key]) == 2 and len(last_events[key]) == 2):
+                scale_picture(last_events[key], current_events_dict[key], get_sprite(key))
+                rotate_picture(last_events[key], current_events_dict[key], get_sprite(key))
+            elif(len(current_events_dict[key]) > 2):
+                print('you should use 2 fingers per sprite at maximum')
 
-def check_events_in_sprite():
-    global sprites, current_events, last_events
+'''
+Helper function to return the sprite object that belongs to the id that is given
+'''
+def get_sprite(sprite_id):
     for sprite in sprites:
-        event_list = []
-        if (len(last_events[id(sprite)]) != 0):
-            for event in current_events:
-                if (check_event_in_sprite(event, sprite)):
-                    event_list.append(event)
-            if (len(event_list) == len(last_events[id(sprite)])):
-                if (len(event_list) == 1):
-                    move_picture(last_events[id(sprite)]
-                                 [0], event_list[0], sprite)
-                if (len(event_list) == 2):
-                    # scale_picture(last_events[id(sprite)], event_list, sprite)
-                    rotate_picture(last_events[id(sprite)], event_list, sprite)
-                if (len(event_list) > 2):
-                    print('you should use 2 fingers per sprite at maximum')
-            last_events[id(sprite)] = event_list
-            return
-        for event in current_events:
-            if (check_event_in_sprite(event, sprite)):
-                event_list.append(event)
-            last_events[id(sprite)] = event_list
+        if(id(sprite) == sprite_id):
+            return sprite     
 
-
-def check_event_in_sprite(event, sprite):
-    if (event.type == 'touch'):
-        if (event.x < sprite.x + sprite.width and event.x > sprite.x):
-            if (event.y < sprite.y + sprite.height and event.y > sprite.y):
-                return True
-    return False
-
-
+'''
+Method to move a given picture at the distance of two events
+'''
 def move_picture(first_event, second_event, sprite):
     dx = second_event.x - first_event.x
     dy = second_event.y - first_event.y
+    # used to be more robust against false move detections
+    if(dx > MOVE_REJECTION or dy > MOVE_REJECTION):
+        return
     new_x = sprite.x + dx
     new_y = sprite.y + dy
     sprite.update(x=new_x, y=new_y)
 
-
+'''
+This method requires two event-lists (two events that exist at the same time) and a sprite.
+It calculates the difference in distance between the two event lists to detect pinch gestures.
+The image is scaled according to the detected distance difference.
+'''
 def scale_picture(first_event_list, second_event_list, sprite):
     previous_distance = calc_distance(first_event_list)
     current_distance = calc_distance(second_event_list)
-    print(f'previous: {previous_distance} and current: {current_distance}')
     pinch_distance = current_distance - previous_distance
 
-    # sprite_diagonal = math.sqrt((sprite.width ** 2) + (sprite.height ** 2))
-    # base_value = sprite_diagonal - (sprite_diagonal * (sprite.scale - 1))
-    # scale = (sprite_diagonal + pinch_distance) / base_value
     scale = sprite.scale
     if (pinch_distance > 0):
-        scale += 0.005
+        scale += SCALING_FACTOR
     elif (pinch_distance < 0):
-        scale -= 0.005
+        scale -= SCALING_FACTOR
     sprite.update(scale=scale)
 
-
-def rotate_picture(first_event_list, second_event_list, sprite):
-    # midpoint_x = ((first_event_list[0].x - first_event_list[1].x) / 2) + first_event_list[1].x
-    # midpoint_y = ((first_event_list[0].y - first_event_list[1].y) / 2) + first_event_list[1].y
-
-    # a = calc_distance([first_event_list[0], Event('helper_event', midpoint_x, midpoint_y)])
-    # c = calc_distance([second_event_list[0], Event('helper_event', midpoint_x, midpoint_y)])
-    # print(f'a: {a} and c: {c}, distance: {a/c}')
-
-    # rotation_angle = math.acos(a/c)
-    first_vector, second_vector = get_vectors(
-        first_event_list, second_event_list)
-
-    dot_product = first_vector[0] * second_vector[0] + \
-        first_vector[1] * second_vector[1]
-    amount_first_vector = math.sqrt(
-        (first_vector[0] ** 2) + first_vector[1] ** 2)
-    amount_second_vector = math.sqrt(
-        (second_vector[0] ** 2) + second_vector[1] ** 2)
-
-    print(f'first vector: {first_vector} and second vector: {second_vector}')
-    print(
-        f'dot_product: {dot_product}, amount first vecotr: {amount_first_vector} and amout second vector: {amount_second_vector}')
-    product_amount = amount_first_vector * amount_second_vector
-    print(f'product amount {product_amount}')
-
-    print(f'division: {dot_product/product_amount}')
-    if (dot_product/product_amount >= 1):
-        return
-
-    rotation_angle = math.acos(dot_product/product_amount)
-    sprite.update(rotation=sprite.rotation + rotation_angle)
-
-
-def get_vectors(events_first_vector, events_second_vector):
-    first_vector = []
-    first_x = events_first_vector[1].x - events_first_vector[0].x
-    first_y = events_first_vector[1].y - events_first_vector[0].y
-    first_vector.append(first_x)
-    first_vector.append(first_y)
-
-    second_vector = []
-    second_x = (events_second_vector[1].x - events_first_vector[0].x) - (
-        events_second_vector[0].x - events_first_vector[0].x)
-    second_y = (events_second_vector[1].y - events_first_vector[0].y) - (
-        events_second_vector[0].y - events_first_vector[0].y)
-    second_vector.append(second_x)
-    second_vector.append(second_y)
-
-    return first_vector, second_vector
-
-
+'''
+Helper function to calculate the distance between two events in a event list
+'''
 def calc_distance(event_list):
     first_event = event_list[0]
     second_event = event_list[1]
@@ -261,6 +253,33 @@ def calc_distance(event_list):
     d = math.sqrt(d_square)
     return d
 
+'''
+This method requires two event-lists (two events that exist at the same time) and a sprite.
+It calculates the angle between these two event list and rotates the given sprite accordingly
+'''
+def rotate_picture(first_event_list, second_event_list, sprite):
+    rotation_angle_rad = get_angle(first_event_list, second_event_list)
+    if(rotation_angle_rad > 1 or rotation_angle_rad < -1):
+        return
+    rotation_angle = -math.degrees(rotation_angle_rad)
+    sprite.update(rotation=sprite.rotation + rotation_angle)
+
+'''
+Helper function to calculate the angle between the two event lists.
+The code is based on: https://stackoverflow.com/questions/2663570/how-to-calculate-both-positive-and-negative-angle-between-two-lines
+'''
+def get_angle(first_event_list, second_event_list):
+
+    a = first_event_list[1].x - first_event_list[0].x
+    b = first_event_list[1].y - first_event_list[0].y
+    c = second_event_list[1].x - second_event_list[0].x
+    d = second_event_list[1].y - second_event_list[0].y
+
+
+    atanA = math.atan2(a, b)
+    atanB = math.atan2(c, d)
+
+    return atanA - atanB
 
 if __name__ == '__main__':
     init()
